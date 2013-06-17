81e59eb97b396a4c14b3213a5c0fe6af971cd58a
KoharaKazuya <reflect.results@gmail.com>
2013-06-17 20:46:58 +0900

    popup にタブツリーを表示するように

--- src/popup.js
+++ src/popup.js
@@ -6,4 +6,6 @@ document.getElementById("newTask").addEventListener('click', bg.newTask, false);
 document.getElementById("startTask").addEventListener('click', bg.startTask, false);
 document.getElementById("later").addEventListener('click', bg.later, false);
 
+document.getElementById("treeView").innerHTML = bg.root.toString().replace(/\n/g, "<br>");
+
 }, false);
